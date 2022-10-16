3922031351b427325af354adc8023468268083e3
JeongRon <83465749+JeongRon@users.noreply.github.com>
2022-10-16 17:08:09 +0900

    # 5.6 Job Posts
    전체 html text -> 원하는 데이터 추출
    - soup (text)
    - jobs (section 태그의 class가 jobs인것)
    - for job_section in jobs:
    - job_posts (job_section에서 li인것)
    - for post in job_posts:

--- main.py
+++ main.py
@@ -9,12 +9,10 @@ if response.status_code != 200:
     print("Can't request website")
 else:
     soup = BeautifulSoup(response.text, "html.parser")
-    print(soup.find_all("section", class_="jobs"))
-
-"""
-Keyword Arguments / 개념 설명
-1. 함수의 arguments 들은 순서에 알맞게 들어가야 함
-2. 하지만, keyword를 넣으면, 순서에 알맞지 않고도 들어갈 수 있다. 
-ex) say_hello(age=27, name="nico") == say_hello("nico", 27)
-따라서 위 코드와 같은 class_=  : keyword Arguments 라는 것을 이해 하기
-"""
+    jobs = soup.find_all("section", class_="jobs")
+    for job_section in jobs:
+        job_posts = job_section.find_all("li")
+        job_posts.pop()
+        for post in job_posts:
+            print(post)
+            print("////////////")
