5f6f5d5aaefac69583f2f798e81df18dc5ad9cd1
JeongRon <83465749+JeongRon@users.noreply.github.com>
2022-10-25 19:56:15 +0900

    #6.2 Render Template
    Flask UI 사용 순서
    - import
    - app = Flask("**")
    - @app.route("/")
    - def fucntion(): (라우터와 붙어있으면, 바로 실행)
    - app.run("0.0.0.0")
    
    flask / render_template 사용
    - flask 는 자동적으로 main.py 폴더 옆에 있는 templates 폴더를 찾아서 render (폴더 위치 중요 함)
    - render_template("home.html") / templates 폴더 안의 home.html 파일 렌더
    - name="nico" / 변수를 지정해서 템플릿 안에 변수 값을 변경도 가능!

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("JobScrapper")
 
 
 @app.route("/")
 def home():
-    return "<h1>hey there</h1><a href='/hello'>go to hello</a>!"
+    return render_template("home.html", name="nico")
 
 
 @app.route("/hello")
