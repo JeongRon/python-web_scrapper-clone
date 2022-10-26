98c90e287c832e42f861239619534fa3c8593fe7
JeongRon <83465749+JeongRon@users.noreply.github.com>
2022-10-26 20:23:42 +0900

    #6.9 File Download
    1. flask - redirect 사용
    - redirect는 URL 이동시켜주는 메서드?
    
    2. flask - send_file 사용
    - 파일을 다운로드 할 때 사용 함
    - return send_file(f"{keyword}.csv", as_attachment=True)
    
    <<app.route("/export) / export URL 만들기>>
    1. keyword 가져오기
    2. keyword가 없으면? -> redirect("/")  다시 home으로 이동
    3. keyword가 db에 없으면? -> redirect(f"/search?keyword={keyword}") search페이지 이동
    - search 페이지 이동하면, db 안에 keyword가 없기에 원하는 데이터 추출작업 후, 화면이 띄어질 것
    4. file.py 에서 save_to_file() 함수 호출 (keyword에 맞는 data들 read/write/close 될 것)
    5.  return send_file(f"{keyword}.csv", as_attachment=True)
    - return send_file() 하면, 웹사이트에서 다운로드가 됨
    
    <<search.html 템플릿>>
    - anchor /export 추가하기

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
 
 app = Flask("JobScrapper")
 
@@ -15,6 +16,8 @@ def home():
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
     if keyword in db:
         jobs = db[keyword]
     else:
@@ -23,6 +26,17 @@ def search():
         jobs = indeed + wwr
         db[keyword] = jobs
     return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
 
 
 app.run("127.0.0.1")
